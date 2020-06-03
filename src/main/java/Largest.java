018dc571614745397c2e9d64a53ad49a8756f22a
stella-Lee <jihyun3030@gmail.com>
2020-06-03 21:16:29 +0900

    min=>max value

--- src/main/java/Largest.java
+++ src/main/java/Largest.java
@@ -1,7 +1,7 @@
 
 public class Largest {
 	public static int largest(int [] list) {
-		int index, max = Integer.MIN_VALUE;
+		int index, max = Integer.MAX_VALUE;
 		if(list.length==0)
 			throw new IllegalArgumentException("Array is Empty");
 		for(index=0;index<=list.length-1;index++) {
